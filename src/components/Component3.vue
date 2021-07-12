b75c04270e069cb6107a71769452d9a3e4e07000
jay <dj.min43@gmail.com>
2021-07-12 09:58:41 +0900

    final

--- src/components/Component3.vue
+++ src/components/Component3.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
       <Component4 />
+      <History />
   </div>
 </template>
 
 <script>
 import Component4 from './Component4.vue'
+import History from './History.vue'
 
 export default {
     components: {
-        Component4
+        Component4,
+        History
     }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -6,22 +6,36 @@ import axios from 'axios'
 const store = createStore({
     state() {
         return {
-            counter: 10,
+            counter: 0,
+            history: [0],
         }
     },
     // mutations should never have async code
     mutations: {
         addToCounter(state, payload) {
             state.counter = state.counter + payload;
+            state.history.push(state.counter)
         },
         subtractFromCounter(state, payload) {
             state.counter = state.counter - payload
+            state.history.push(state.counter)
         }
     },
     actions: {
         async addRandomNumber(context) {
             let data = await axios.get('https://www.random.org/integers/?num=1&min=-1000&max=1000&col=1&base=10&format=plain&rnd=new')
             context.commit('addToCounter', data.data)
+        }
+    },
+    getters: {
+        activeIndexes: (state) => (payload) => {
+            let indexes = [];
+            state.history.forEach((number, index) => {
+                if (number === payload) {
+                    indexes.push(index)
+                }
+            })
+            return indexes
         }
     }
 })
